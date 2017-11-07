ec42d006463851b259efc17edf38d40421e8c80b
jean-felipe <mandakeru@gmail.com>
2017-11-06 23:32:10 -0200

    migrations runned, forget docker it not work

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  price       :decimal(, )
+#  max_bargain :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryGirl.define do
   factory :product do
     title "MyString"
